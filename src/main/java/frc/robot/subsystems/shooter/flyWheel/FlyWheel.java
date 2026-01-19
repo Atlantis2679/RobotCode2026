a6b84725ec8c530279712f82220501a9764b64aa
Hillel Shiff <hillel.sh2021@leyada.net>
2026-01-19 20:37:03 +0200

    finish flyWheel code

--- src/main/java/frc/robot/subsystems/shooter/flyWheel/FlyWheel.java
+++ src/main/java/frc/robot/subsystems/shooter/flyWheel/FlyWheel.java
@@ -1,11 +1,19 @@
 package frc.robot.subsystems.shooter.flyWheel;
 
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
 import frc.robot.subsystems.shooter.flyWheel.io.*;
+import frc.robot.subsystems.shooter.flyWheel.FlyWheelConstants;
 import team2679.atlantiskit.helpers.RotationalSensorHelper;
 import team2679.atlantiskit.logfields.LogFieldsTable;
+import team2679.atlantiskit.tunables.extensions.TunableArmFeedforward;
+import team2679.atlantiskit.tunables.extensions.TunableTrapezoidProfile;
+import team2679.atlantiskit.valueholders.ValueHolder;
 
 public class FlyWheel extends SubsystemBase{
     
@@ -15,10 +23,35 @@ public class FlyWheel extends SubsystemBase{
         new FlyWheelIOSim(fieldsTable);
 
     private final RotationalSensorHelper sensorHelper;
-    
+
+    private PIDController flyWheelPidController = new PIDController(
+        FlyWheelConstants.KP,
+        FlyWheelConstants.KI,
+        FlyWheelConstants.KD
+    );
+    private final TunableTrapezoidProfile flyWheelTrapezoid = new TunableTrapezoidProfile(
+        new TrapezoidProfile.Constraints(
+            FlyWheelConstants.MAX_VELOCITY_DEG_PER_SEC,
+            FlyWheelConstants.MAX_ACCELERATION_DEG_PER_SEC_SQUEARD)        
+    );
+    private TunableArmFeedforward flyWheelFeedforward = Robot.isSimulation()?
+        new TunableArmFeedforward(
+            FlyWheelConstants.Sim.SIM_KS,
+            FlyWheelConstants.Sim.SIM_KG,
+            FlyWheelConstants.Sim.SIM_KV,
+            FlyWheelConstants.Sim.SIM_KA
+            ):
+        new TunableArmFeedforward(
+            FlyWheelConstants.KS,
+            FlyWheelConstants.KG,
+            FlyWheelConstants.KV,
+            FlyWheelConstants.KA
+    );
+
+
     public FlyWheel() {
         fieldsTable.update();
-        
+
         sensorHelper = new RotationalSensorHelper(io.flywheelMotorAbsoluteRotations.getAsDouble(), 0);
     }
 
@@ -42,8 +75,23 @@ public class FlyWheel extends SubsystemBase{
     public void setVoltage(double volt){
         io.setVoltage(volt);
     }
+    public void setRPM(double desireRPM){
+
+    }
+    public TrapezoidProfile.State calculateTrapezoidProfile(
+        double time, TrapezoidProfile.State initialState, TrapezoidProfile.State desiredState){
+            return flyWheelTrapezoid.calculate(time, initialState, desiredState);
+    }
+    public double CalcVolts(double desiredSpeed, double desiredExceleration){
+        double exceleration = flyWheelFeedforward.calculate(desiredSpeed, desiredExceleration);
+        exceleration += flyWheelPidController.calculate(getVelocity(), desiredSpeed);
+        return exceleration;
+    }
+
     public void stop(){
         io.setVoltage(0);
     }
-    
+    public void resetPID(){
+        flyWheelPidController.reset();
+    }
 }
