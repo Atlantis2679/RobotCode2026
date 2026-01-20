b7638aa9cbcaa2f2a47e55ca0afc9832fae4d9e7
YonatanBrot <yonatanbrot@gmail.com>
2026-01-20 17:18:08 +0200

    Full Index.java code

---
+++ src/main/java/frc/robot/subsystems/index/Index.java
@@ -0,0 +1,37 @@
+package frc.robot.subsystems.index;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
+import frc.robot.subsystems.index.io.*;
+import team2679.atlantiskit.logfields.LogFieldsTable;
+
+public class Index extends SubsystemBase{
+    private final LogFieldsTable fields = new LogFieldsTable(getName());
+    private final IndexIO io = Robot.isReal() ? new IndexIOSparkMax(fields) : new IndexIOSim(fields);
+    
+    public Index(){}
+
+    @Override
+    public void periodic(){
+        fields.recordOutput("Current command", 
+            getCurrentCommand() != null ? getCurrentCommand().getName() : "None");
+    }
+
+    //Input:
+    public void setSpindexVolt(double volt){
+        fields.recordOutput("Spin Motor volt", volt);
+        io.setSpindexVolt(volt);
+    }
+    public void setIndexerVolt(double volt){
+        fields.recordOutput("In Motor Volt", volt);
+        io.setIndexerVolt(volt);
+    }
+
+    //Output:
+    public double getSpindexCurrent() {
+        return io.getSpinCurrent.getAsDouble();
+    }
+    public double getIndexerCurrent() {
+        return io.getInCurrent.getAsDouble();
+    }
+}
