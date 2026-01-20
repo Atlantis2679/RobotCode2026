e3ccdfa9df403aff006a2a34fe096d71078fc167
YonatanBrot <yonatanbrot@gmail.com>
2026-01-20 16:26:55 +0200

    Same as last commit

--- src/main/java/frc/robot/subsystems/index/IndexConstants.java
+++ src/main/java/frc/robot/subsystems/index/IndexConstants.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems.index;
 
 public class IndexConstants {
+    public static final int SpinCurrentLimit = 0;
+    public static final int InCurrentLimit = 0;
     public class Canbus {
         public static final int SPINDEX_ID = 0;
         public static final int INDEXER_ID = 0;
