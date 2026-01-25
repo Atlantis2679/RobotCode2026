241b3c75627528c28977d928b2e30119670282f5
Yair Bligh <yairbligh@gmail.com>
2026-01-25 11:16:10 +0200

    created new branch for hood only

---
+++ src/main/java/frc/robot/subsystems/hood/io/HoodIOSparkMax.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems.hood.io;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import frc.robot.RobotMap.*;
+import team2679.atlantiskit.logfields.LogFieldsTable;
+
+public class HoodIOSparkMax extends HoodIO{
+
+    private final SparkMax hoodMotor = new SparkMax(CANBUS.HOOD_MOTOR_ID, MotorType.kBrushless);
+    private final DutyCycleEncoder hoodEncoder = new DutyCycleEncoder(DIO.HOOD_ENCODER_ID);
+
+    public HoodIOSparkMax(LogFieldsTable fieldsTable){
+        super(fieldsTable);
+    }
+
+    @Override
+    public double getHoodMotorAngle() {
+        return hoodEncoder.get() * 360;
+    }
+
+    @Override
+    public void setVoltage(double volt) {
+        hoodMotor.setVoltage(volt);
+    }
+    
+}
--- src/main/java/frc/robot/subsystems/index/IndexConstants.java
+++
@@ -1,6 +0,0 @@
-package frc.robot.subsystems.index;
-
-public final class IndexConstants {
-    public static final int SPINDEX_CURRENT_LIMIT = 0;
-    public static final int INDEXER_CURRENT_LIMIT = 0;
-}
