217701bf8a62ab4df57f14aed4ae8ea484cb0cf1
Yonatan Vigiser <yonatanvigiser@gmail.com>
2026-01-26 18:07:36 +0200

    Fix merge conflicts in hood

---
+++ src/main/java/frc/robot/subsystems/hood/HoodConstants.java
@@ -0,0 +1,42 @@
+package frc.robot.subsystems.hood;
+
+public final class HoodConstants {
+    
+    public static final double MAX_VELOCITY_DEG_PER_SEC = 360 * 0;
+    public static final double MAX_ACCELERATION_DEG_PER_SEC_SQUEARD = 0;
+
+    public static final int HOOD_MAX_VOLTAGE = 12;
+
+    public static final double MAX_ANGLE_DEGREES = 0;
+    public static final double MIN_ANGLE_DEGREES = 0;
+    public static final double UPPER_BOUND = 0;
+    public static final double LOWER_BOUND = 0;
+
+    public static final double ANGLE_TOLERENCE_DEGREES = 0;
+
+    public static final double KP = 0;
+    public static final double KI = 0;
+    public static final double KD = 0;
+
+    public static final double KS = 0;
+    public static final double KA = 0;
+    public static final double KV = 0;
+    public static final double KG = 0;
+
+    public static final double ENCODER_CONNECTED_DEBAUNCER_SEC = 0.1;
+    public static final double ANGLE_OFFSET = 0;
+
+    public final class Sim{
+        public static final double SIM_KS = 0;
+        public static final double SIM_KA = 0;
+        public static final double SIM_KV = 0;
+        public static final double SIM_KG = 0;
+
+        public static final double JOINT_GEAR_RATIO = 0/1;
+        public static final double JKG_METERS_SQUEARED = 0;
+        public static final double TURNING_MIN_DEGREES = 0;
+        public static final double TURINIG_MAX_DEGREES = 0;
+        public static final double ARM_LENGTH_M = 0.0;
+    }
+
+}
