dbd69a9208f934d410e9527dc5f520e22f761971
YonatanBrot <yonatanbrot@gmail.com>
2026-01-29 11:15:34 +0200

    Added configure Operator, with support for the currently written commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,12 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.allCommands.AllCommands;
+import frc.robot.subsystems.flywheel.FlyWheel;
+import frc.robot.subsystems.hood.Hood;
+import frc.robot.subsystems.index.Index;
+import frc.robot.subsystems.intake.roller.Roller;
+import frc.robot.subsystems.intake.slapdown.Slapdown;
 import frc.robot.subsystems.swerve.Swerve;
 import frc.robot.subsystems.swerve.SwerveCommands;
 import frc.robot.utils.NaturalXboxController;
@@ -13,9 +19,15 @@ import team2679.atlantiskit.tunables.extensions.TunableCommand;
 
 public class RobotContainer {
     private final Swerve swerve = new Swerve();
+    private final Slapdown slapdown = new Slapdown();
+    private final Roller roller = new Roller();
+    private final Index index = new Index();
+    private final Hood hood = new Hood();
+    private final FlyWheel flyWheel = new FlyWheel();
 
     private final SwerveCommands swerveCommands = new SwerveCommands(swerve);
-
+    private final AllCommands allCommands =new AllCommands(slapdown, roller, flyWheel, hood, swerve, index);
+    
     private final NaturalXboxController driverController = new NaturalXboxController(
             RobotMap.Controllers.DRIVER_PORT);
 
@@ -44,6 +56,17 @@ public class RobotContainer {
                         driverController::getRightY).fullTunable());
 
         driverController.a().onTrue(new InstantCommand(() -> swerve.resetYaw(0)));
+    }
+
+    public void configureOperator() {
+        driverController.a()
+            .onTrue(allCommands.startIntake())
+            .onFalse(allCommands.stopIntake());
+        driverController.leftBumper().whileTrue(allCommands.tunableDeliveryPrep());
+        driverController.rightBumper().whileTrue(allCommands.tunableScorePrep());
+
+        TunablesManager.add("Score Commad", allCommands.tunableScorePrep().fullTunable());
+        TunablesManager.add("Delivery Commad", allCommands.tunableDeliveryPrep().fullTunable());
     }
 
     public void enterSwerveIntoTest() {
