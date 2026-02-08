f6e589849f1180f4124508a40665f2b57ebdf11f
Yonatan Vigiser <yonatanvigiser@gmail.com>
2026-02-08 15:55:04 +0200

    Update vendordeps & Update constants

--- src/main/java/frc/robot/subsystems/flywheel/FlyWheelConstants.java
+++ src/main/java/frc/robot/subsystems/flywheel/FlyWheelConstants.java
@@ -1,24 +1,23 @@
 package frc.robot.subsystems.flywheel;
 
 public class FlyWheelConstants {
-    public static final double MAX_VOLTAGE = 12;
+    public static final double MAX_VOLTAGE = 10;
     public static final double STATOR_CURRENT_LIMIT = 90;
-    public static final double SUPPLY_CURRENT_LIMIT = 40;
-    public static final double SUPPLY_CURRENT_LOWER_LIMIT = 0;
-    public static final double SUPPLY_CURRENT_LOWER_TIME = 0;
+    public static final double SUPPLY_CURRENT_LIMIT = 60;
+    public static final double SUPPLY_CURRENT_LOWER_LIMIT = 40;
+    public static final double SUPPLY_CURRENT_LOWER_TIME = 2;
 
-    public static final double SPEED_TOLERANCE_RPM = 0.5;
+    public static final double SPEED_TOLERANCE_RPM = 50;
 
     public static final double KP = 7;
     public static final double KI = 0.02;
     public static final double KD = 0.01;
 
-    public static final double KS = 0;
-    public static final double KA = 0;
-    public static final double KV = 0;
-    public static final double KG = 0;
+    public static final double KS = 0.01;
+    public static final double KA = 1;
+    public static final double KV = 1;
 
-    public static final double GEAR_RATIO = 2;
+    public static final double GEAR_RATIO = 1;
 
     public static class Sim {
         public static final double SIM_KS = 0;
