6fc082498ff467c2532c943d8ccdcc21f44d7fb9
YonatanBrot <yonatanbrot@gmail.com>
2026-02-12 18:37:16 +0200

    Removed the useless stuff

--- src/main/java/frc/robot/subsystems/swerve/Swerve.java
+++ src/main/java/frc/robot/subsystems/swerve/Swerve.java
@@ -93,12 +93,6 @@ public class Swerve extends SubsystemBase implements Tunable {
 
     fieldsTable.recordOutput("Is red alliance", isRedAlliance());
     SmartDashboard.putBoolean("isRedAlliance", isRedAlliance());
-
-    fieldsTable.recordOutput("Flat Acceleration", getFlatAcceleration());
-    fieldsTable.recordOutput("X Signed Acceleration", getFlatAccelerationXSign());
-    fieldsTable.recordOutput("Y Signed Acceleration", getFlatAccelerationYSign());
-    fieldsTable.recordOutput("X Acceleration Sign", getXAccelerationSign());
-    fieldsTable.recordOutput("Y Acceleration Sign", getYAccelerationSign());
   }
 
   public void drive(double vxSpeedMPS, double vySpeedMPS, double vAngleRandiansPS, boolean isFieldRelative,
@@ -194,34 +188,6 @@ public class Swerve extends SubsystemBase implements Tunable {
       currents[i] = modules[i].getCurrent();
     }
     return currents;
-  }
-  
-  public double getFlatAcceleration() {
-    return Math.sqrt(
-      Math.pow(getXAcceleration(), 2)
-      + Math.pow(getYAcceleration(), 2));
-  }
-
-  public double getXAccelerationSign() {
-    if (getXAcceleration()==0) {
-      return 0;
-    }
-    return Math.abs(getXAcceleration())/getXAcceleration();
-  }
-
-  public double getYAccelerationSign() {
-    if (getYAcceleration()==0) {
-      return 0;
-    }
-    return Math.abs(getYAcceleration())/getYAcceleration();
-  }
-
-  public double getFlatAccelerationXSign() {
-    return getFlatAcceleration()*getXAccelerationSign();
-  }
-
-  public double getFlatAccelerationYSign() {
-    return getFlatAcceleration()*getYAccelerationSign();
   }
 
   @Override
