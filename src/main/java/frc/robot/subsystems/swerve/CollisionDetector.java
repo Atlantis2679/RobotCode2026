36aab981afedb39d46c67ab2dd6b1cf35b202d85
YonatanBrot <yonatanbrot@gmail.com>
2026-02-15 18:08:18 +0200

    Added Tunables

--- src/main/java/frc/robot/subsystems/swerve/CollisionDetector.java
+++ src/main/java/frc/robot/subsystems/swerve/CollisionDetector.java
@@ -2,14 +2,22 @@ package frc.robot.subsystems.swerve;
 
 import static frc.robot.subsystems.swerve.SwerveConstants.CollisionDetectorConstants.*;
 import team2679.atlantiskit.logfields.LogFieldsTable;
+import team2679.atlantiskit.tunables.Tunable;
+import team2679.atlantiskit.tunables.TunableBuilder;
+
 import static java.lang.Math.*;
 
-public class CollisionDetector {
+public class CollisionDetector implements Tunable {
     private double lastXAcceleration = 0;
     private double lastYAcceleration = 0;
     private boolean inCollision = false;
 
     private LogFieldsTable logFieldsTable;
+
+    private double zeroAcceleration = ZERO_ACCELERATION;
+    private double minCollisionAcceleration = MIN_COLLISION_ACCELERATION;
+    private double maxCollisionAcceleration = MAX_COLLISION_ACCELERATION;
+    private double majorJerk = MAJOR_JERK;
     
     public CollisionDetector(LogFieldsTable logFieldsTable) {
         this.logFieldsTable = logFieldsTable;
@@ -23,11 +31,11 @@ public class CollisionDetector {
     }
 
     public boolean check(CollisionDetectorInfo info){
-        if (info.zAcceleration >= ZERO_ACCELERATION) {
+        if (info.zAcceleration >= zeroAcceleration) {
             update(info, true);
             return true;
         } else if (!inCollision) {
-            if (abs(info.xAcceleration)>ZERO_ACCELERATION&&abs(info.yAcceleration)>ZERO_ACCELERATION) {
+            if (abs(info.xAcceleration)>zeroAcceleration&&abs(info.yAcceleration)>zeroAcceleration) {
                 double currentMax1 = 0, currentMax2 = 0;
                 for (double current : info.currents) {
                     if (current>currentMax1) {
@@ -39,24 +47,31 @@ public class CollisionDetector {
                         }
                     }
                 }
-                boolean toReturn = MIN_COLLISION_ACCELERATION<abs(currentMax1)
-                    &&abs(currentMax2)<MAX_COLLISION_ACCELERATION;
+                boolean toReturn = minCollisionAcceleration<abs(currentMax1)
+                    &&abs(currentMax2)<maxCollisionAcceleration;
                 update(info, toReturn);
                 return toReturn;
             } else {
-                boolean toReturn = abs(info.xAcceleration-lastXAcceleration)>=MAJOR_JERK
-                    ||abs(info.yAcceleration-lastYAcceleration)>=MAJOR_JERK;
+                boolean toReturn = abs(info.xAcceleration-lastXAcceleration)>=majorJerk
+                    ||abs(info.yAcceleration-lastYAcceleration)>=majorJerk;
                 update(info, toReturn);
                 return toReturn;
             }
         } else {
-            boolean toReturn = MIN_COLLISION_ACCELERATION<=info.xAcceleration
-                    &&info.xAcceleration<=MAX_COLLISION_ACCELERATION
-                &&MIN_COLLISION_ACCELERATION<=info.yAcceleration
-                    &&info.yAcceleration<=MAX_COLLISION_ACCELERATION;
+            boolean toReturn = minCollisionAcceleration<=info.xAcceleration
+                    &&info.xAcceleration<=maxCollisionAcceleration
+                &&minCollisionAcceleration<=info.yAcceleration
+                    &&info.yAcceleration<=maxCollisionAcceleration;
             update(info, toReturn);
             return toReturn;
         }
+    }
+
+    public void initTunable(TunableBuilder tunableBuilder) {
+        tunableBuilder.addDoubleProperty("Zero Acceleration", () -> zeroAcceleration, (a) -> this.zeroAcceleration = a);
+        tunableBuilder.addDoubleProperty("Min Collision Acceleration", () -> minCollisionAcceleration, (a) -> minCollisionAcceleration = a);
+        tunableBuilder.addDoubleProperty("Max Collision Acceleration", () -> maxCollisionAcceleration, (a) -> maxCollisionAcceleration = a);
+        tunableBuilder.addDoubleProperty("Major Jerk", () -> majorJerk, (a) -> majorJerk = a);
     }
     
     public static record CollisionDetectorInfo(double xAcceleration, double yAcceleration,
