6cfd0932492cfc09307ae46652672b281878e918
Yonatan Vigiser <yonatanvigiser@gmail.com>
2026-02-23 20:53:02 +0200

    Remove changes in Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -48,22 +48,22 @@ public class Robot extends LoggedRobot {
 
     private void initializeAdvantageKit() {
         Logger.recordMetadata("RuntimeType", getRuntimeType().toString());
-        // Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
-        // Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
-        // Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
-        // Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
-        // Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
-        // switch (BuildConstants.DIRTY) {
-        //     case 0:
-        //         Logger.recordMetadata("GitDirty", "All changes committed");
-        //         break;
-        //     case 1:
-        //         Logger.recordMetadata("GitDirty", "Uncomitted changes");
-        //         break;
-        //     default:
-        //         Logger.recordMetadata("GitDirty", "Unknown");
-        //         break;
-        // }
+        Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
+        Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+        Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+        Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+        Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+        switch (BuildConstants.DIRTY) {
+            case 0:
+                Logger.recordMetadata("GitDirty", "All changes committed");
+                break;
+            case 1:
+                Logger.recordMetadata("GitDirty", "Uncomitted changes");
+                break;
+            default:
+                Logger.recordMetadata("GitDirty", "Unknown");
+                break;
+        }
 
         if (getIsReplay()) {
             System.out.println("******************* Starting replay mode! *******************");
