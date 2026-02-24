dcd7b05017b35c8f45c1cfcc86d6e8aeab6095ca
YonatanBrot <yonatanbrot@gmail.com>
2026-02-24 20:29:24 +0200

    final version (pls)

--- src/main/java/frc/robot/subsystems/poseestimation/CollisionDetector.java
+++ src/main/java/frc/robot/subsystems/poseestimation/CollisionDetector.java
@@ -22,43 +22,40 @@ public class CollisionDetector {
         this.logFieldsTable = logFieldsTable;
     }
 
-    private boolean updateWrapper(CollisionDetectorInfo info, boolean inCollision) {
-        logFieldsTable.recordOutput("X Jerk", abs(lastXAcceleration-info.xAcceleration));
-        logFieldsTable.recordOutput("Y Jerk", abs(lastYAcceleration-info.yAcceleration));
-        lastXAcceleration = info.xAcceleration();
-        lastYAcceleration = info.yAcceleration();
-        this.inCollision = inCollision;
-        logFieldsTable.recordOutput("In Collision?", inCollision);
-        return inCollision;
-    }
-
     private void updateAVGs(double[] vals) {
         highCurreentAvarage.update(vals[0]);
         lowCurreentAvarage.update(vals[1]);
     }
 
-    private void updateAVGs() {
+    private void resetAVGs() {
         highCurreentAvarage.reset();
         lowCurreentAvarage.reset();
     }
 
-    public boolean check(CollisionDetectorInfo info) {
+    public void update(CollisionDetectorInfo info) {
         if (info.zAcceleration >= Z_ACCELERATION_THRESHOLD) {
-            updateWrapper(info, false);
-            updateAVGs();
-            return true;
+            resetAVGs();
+            inCollision = true;
         } if (inCollision || (info.xAcceleration<=STATIC_ACCELERATION_THRESHOLD
                 &&info.yAcceleration<=STATIC_ACCELERATION_THRESHOLD)) {
             double[] currents = getHighestX(2, info.currents);
             updateAVGs(currents);
-            return updateWrapper(info, highCurreentAvarage.get()>=HIGH_CURRENT_COLLISION_THRESHOLD
-                &&lowCurreentAvarage.get()>=LOW_CURRENT_COLLISION_THRESHOLD);
+            inCollision = highCurreentAvarage.get() >= HIGH_CURRENT_COLLISION_THRESHOLD
+                &&lowCurreentAvarage.get() >= LOW_CURRENT_COLLISION_THRESHOLD;
         } else {
-            boolean toReturn = abs(lastXAcceleration-info.xAcceleration)>=JERK_COLLISION_THRESHOLD
+            inCollision = abs(lastXAcceleration-info.xAcceleration)>=JERK_COLLISION_THRESHOLD
                 ||abs(lastYAcceleration-info.yAcceleration)>=JERK_COLLISION_THRESHOLD;
-            updateAVGs();
-            return updateWrapper(info, toReturn);
+            resetAVGs();
         }
+        logFieldsTable.recordOutput("X Jerk", abs(lastXAcceleration-info.xAcceleration));
+        logFieldsTable.recordOutput("Y Jerk", abs(lastYAcceleration-info.yAcceleration));
+        lastXAcceleration = info.xAcceleration();
+        lastYAcceleration = info.yAcceleration();
+        logFieldsTable.recordOutput("In Collision?", inCollision);
+    }
+
+    public boolean inCollision() {
+        return inCollision;
     }
 
     public static record CollisionDetectorInfo(double xAcceleration, double yAcceleration,
